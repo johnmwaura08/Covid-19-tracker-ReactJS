303174e49e954a4c1cb95f8f16d98bdd12680299
John Mwaura <johnmwaura08@gmail.com>
2020-05-14 13:21:10 -0500

    added an async component did mount to call the api function from api folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Cards, Chart, CountryPicker } from "./components"; // check index.js file from components folder to see the export
 
-export default class App extends Component {
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends Component {
+  async componentDidMount() {
+    // this is where we fetch Data
+
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render() {
     return (
-      <div>
-        <h1>something</h1>
-
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
-    )
+    );
   }
 }
+
+export default App;
