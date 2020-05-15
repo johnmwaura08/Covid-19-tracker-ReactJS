ec38bb486772221e5b21f8ace849555bbcc752d3
John Mwaura <johnmwaura08@gmail.com>
2020-05-14 20:17:23 -0500

    code for the linechart showing deaths and infected globally

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+//we're using hooks here
 
- const Chart = () => {
-    return (
-        <h1>Chart</h1>
-    )
-}
+import { fetchDailyData } from "../../api";
+import styles from "./Chart.module.css";
+import { Line, Bar } from "react-chartjs-2";
+
+const Chart = () => {
+  const { dailyData, setDailyData } = useState({});
+
+  //we cant use async infront of useeffect so we'll have to create an async function and put it inside
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    console.log(dailyData);
+
+    fetchApi();
+  });
+
+  //responsible for global showing of data
+  //ternary operator means if daily data is available we'll show or else show null
+  //our api only brings daily data for infected and deaths
+  const lineChart = (
+    dailyData.length 
+    ? (
+            <Line
+            data={{
+                labels: dailyData.map(({date}) => date),
+                datasets: [{
+                    data: dailyData.map(({confirmed}) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+
+                },
+                    
+                {
+                    data: dailyData.map(({deaths}) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.5',
+                    fill: true,
+
+
+                    }],
+            }}
+            />
+
+     ) : null
+
+  );
+
+
+
+
+  return (<div className={styles.container}>
+    {lineChart}
+     </div>);
+   
+  
+  
+  
+   };
 export default Chart;
