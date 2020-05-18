2fb09a447925ce34bbd9acde418bc0222e505eab
John Mwaura <johnmwaura08@gmail.com>
2020-05-18 11:40:42 -0500

    added image src

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Cards, Chart, CountryPicker } from "./components"; // check index.js fi
 
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import covidImage from './images/covidimage.png';
 
 class App extends Component {
 
   state ={ //easier way to declare state
 
-    data: {}
+    data: {},
+    country: ''
 
 
 
@@ -23,14 +25,27 @@ class App extends Component {
 
   
   }
+
+  handleCountryChange = async (country)=>{
+
+    //fetch the data
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country});
+
+  }
   render() {
 
-    const { data} = this.state;
+    const { data,country} = this.state;
     return (
       <div className={styles.container}>
+        <div className={styles.imageContainer}>
+
+            <img classname={styles.image}src={covidImage} alt="coronavirus"/>
+          </div>
         <Cards data ={data}/>
-        <CountryPicker />
-        <Chart />
+        <CountryPicker  handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country}/>
       </div>
     );
   }
